109f178e574c219c28438954387b794da38a4350
myeongmy <kp6069@naver.com>
2022-12-16 03:19:40 +0900

    [오류 수정] 회원가입 및 게시글 삭제 오류

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -3,6 +3,7 @@ package com.cos.blog.model;
 import java.sql.Timestamp;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -13,9 +14,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -46,7 +50,9 @@ public class Board {
 	@JoinColumn(name="userId")    // FK 이름을 userId로 설정
 	private User user;    // 작성자   (DB는 객체를 저장할 수 없다. 그래서 FK 사용! 그러나 자바에서는 오브젝트 저장 가능)
 	
-	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER)  // mappedBy는 연관관계의 주인이 아니라는 뜻 (FK 생성 NO...), "board"는 Reply 클래스 안의 해당 필드 값
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)  // mappedBy는 연관관계의 주인이 아니라는 뜻 (FK 생성 NO...), "board"는 Reply 클래스 안의 해당 필드 값
+	@JsonIgnoreProperties({"board"})
+	@OrderBy("id desc")
 	private List<Reply> reply;           // 여기는 joinColumn이 필요 없다. (왜냐면, DB에는 FK가 필요 없고 개발 시 편리함을 위해 만든 필드이기 때문에)
 	
 	@CreationTimestamp
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,26 +1,26 @@
 let index = {
-	init: function(){
-		$("#btn-save").on("click", ()=>{   // function(){}말고 ()=>{} 사용한 이유는 this를 바인딩하기 위해서
+	init: function() {
+		$("#btn-save").on("click", () => {   // function(){}말고 ()=>{} 사용한 이유는 this를 바인딩하기 위해서
 			this.save();
 		});
-		$("#btn-login").on("click", ()=>{
+		$("#btn-login").on("click", () => {
 			this.login();
 		});
-		$("#btn-update").on("click", ()=>{
+		$("#btn-update").on("click", () => {
 			this.update();
 		});
 	},
-	
-	save: function(){
+
+	save: function() {
 		//alert("user의 save 함수 호출됨");
-		let data ={
+		let data = {
 			username: $("#username").val(),
 			password: $("#password").val(),
 			email: $("#email").val()
 		};
-		
+
 		//console.log(data);
-		
+
 		// ajax 호출 시 default가 비동기 호출
 		$.ajax({
 			// 회원가입 수행 요청
@@ -29,23 +29,27 @@ let index = {
 			data: JSON.stringify(data),      //자바스크립트 객체를 JSON으로 변환
 			contentType: "application/json; charset=utf-8",    // 요청 http 바디 데이터가 어떤 타입인지(MIME)
 			dataType: "json"    // 요청에 대한 응답 데이터는 기본적으로 문자열 => "json"이라고 적으면 자바스크립트 오브젝트로 변환해줌
-		}).done(function(resp){   // resp: 자바스크립트 오브젝트 형태
-			alert("회원가입이 완료되었습니다.");
-			location.href = "/";
-		}).fail(function(error){    // http 응답상태가 200이 아닌 경우
+		}).done(function(resp) {   // resp: 자바스크립트 오브젝트 형태
+			if (resp.status === 500) {
+				alert("회원가입에 실패하였습니다.");
+			} else {
+				alert("회원가입이 완료되었습니다.");
+				location.href = "/";
+			}
+		}).fail(function(error) {    // http 응답상태가 200이 아닌 경우
 			alert(JSON.stringify(error));
 		});      // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
 	},
-	
-	login: function(){
+
+	login: function() {
 		//alert("user의 save 함수 호출됨");
-		let data ={
+		let data = {
 			username: $("#username").val(),
 			password: $("#password").val()
 		};
-		
+
 		//console.log(data);
-		
+
 		// ajax 호출 시 default가 비동기 호출
 		$.ajax({
 			// 회원가입 수행 요청
@@ -54,34 +58,34 @@ let index = {
 			data: JSON.stringify(data),      //자바스크립트 객체를 JSON으로 변환
 			contentType: "application/json; charset=utf-8",     // 요청 http 바디 데이터가 어떤 타입인지(MIME)
 			dataType: "json"    // 요청에 대한 응답 데이터는 기본적으로 문자열 => "json"이라고 적으면 자바스크립트 오브젝트로 변환해줌
-		}).done(function(resp){   // resp: 자바스크립트 오브젝트 형태
+		}).done(function(resp) {   // resp: 자바스크립트 오브젝트 형태
 			alert("로그인이 완료되었습니다.");
 			location.href = "/";
-		}).fail(function(error){    // http 응답상태가 200이 아닌 경우
+		}).fail(function(error) {    // http 응답상태가 200이 아닌 경우
 			alert(JSON.stringify(error));
 		});      // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
 	},
-	
-	update: function(){
-		let data ={
+
+	update: function() {
+		let data = {
 			id: $("#id").val(),
 			username: $("#username").val(),
 			password: $("#password").val(),
 			email: $("#email").val()
 		};
-		
+
 		$.ajax({
 			type: "PUT",
 			url: "/user",
-			data: JSON.stringify(data),     
-			contentType: "application/json; charset=utf-8",     
-			dataType: "json"    
-		}).done(function(resp){   
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
 			alert("회원수정이 완료되었습니다.");
 			location.href = "/";
-		}).fail(function(error){   
+		}).fail(function(error) {
 			alert(JSON.stringify(error));
-		});      
+		});
 	}
 }
 
